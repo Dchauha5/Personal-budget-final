9c06b434bc08d257599075b975883dc422904152
Dchauha5 <Dchauha5@uncc.edu>
2020-12-17 00:27:51 -0500

    register page called signup added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
+import { LoginComponent } from './login/login.component';
+import { LogoutComponent } from './logout/logout.component';
+import { SignupComponent } from './signup/signup.component';
+import { ErrorComponent } from './error/error.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    LoginComponent,
+    LogoutComponent,
+    SignupComponent,
+    ErrorComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
